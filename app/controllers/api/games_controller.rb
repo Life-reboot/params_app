05342471dfa9cf64a9ca515cc5b7cba188d5ff2d
Peter Jang <peter.x.jang@gmail.com>
2020-03-03 21:14:15 -0600

    Add query guess number game

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -7,4 +7,17 @@ class Api::GamesController < ApplicationController
     end
     render "name.json.jb"
   end
+
+  def guess_method
+    winning_number = 32
+    input_guess = params["the_users_guess"].to_i
+    if input_guess > winning_number
+      @message = "Pick lower!"
+    elsif input_guess < winning_number
+      @message = "Pick higher!"
+    else
+      @message = "You win!"
+    end
+    render "guess.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     get "/query_params_url" => "params_examples#query_params_action"
 
     get "/name_url" => "games#name_method"
+    get "/guess_url" => "games#guess_method"
   end
 end
